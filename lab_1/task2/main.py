5b75571eddc5695d51be237e47433fe3142833a1
radlEve <fadeeveldar2000@gmail.com>
2025-03-28 12:47:00 +0400

    Добавлены описания функций, исправлены и добавлены описания в константах

--- lab_1/task2/main.py
+++ lab_1/task2/main.py
@@ -4,6 +4,10 @@ from substitution import decrypt
 
 
 def read_encoded_file() -> str:
+    """
+    Прочитать текстовый файл, указанный в константах, и вернуть в виде str
+    :return: строка, содержащая весь текст файла
+    """
     try:
         with open(ENCODED_FILE, 'r', encoding='utf-8') as file:
             return file.read()
@@ -11,6 +15,13 @@ def read_encoded_file() -> str:
         raise FileNotFoundError(f"Файл {ENCODED_FILE} не найден!")
 
 def write_results(decrypted_text: str, key: dict) -> None:
+    """
+    Записать в отдельные файлы (пути до которых указаны в константах)
+        расшифрованный текст и ключ в виде "*char_before* -> *char_after*"
+    :param decrypted_text: расшифрованный текст
+    :param key: ключ для расшифровки исходного текста
+    :return: None
+    """
     try:
         with open(DECRYPTED_FILE, 'w', encoding='utf-8') as file:
             file.write(decrypted_text)
@@ -23,8 +34,16 @@ def write_results(decrypted_text: str, key: dict) -> None:
         raise Exception(f"Ошибка записи в {ENCODED_FILE}: {e}")
 
 
-def apply_manual_corrections(text: str, replacements: list[tuple[str, str]]):
-    results = text
+def apply_manual_corrections(text: str, replacements: list[tuple[str, str]]) -> str:
+    """
+    Применить ручные изменения для текста,
+    поочередно меняя местами по 2 буквы из алфавита
+    :param text: исходный текст
+    :param replacements: список букв,
+        которые необходимо поменять местами в заданном порядке
+    :return: получившийся текст
+    """
+    result = text
     for char1, char2 in replacements:
         result = result.replace(char1, '\0').replace(char2, char1).replace('\0', char2)
     return result
@@ -37,7 +56,7 @@ def main():
 
         decrypted_text, found_key = decrypt(encoded_text)
 
-        apply_manual_corrections(decrypted_text, CHAR_REPLACEMENTS)
+        decrypted_text = apply_manual_corrections(decrypted_text, CHAR_REPLACEMENTS)
 
         write_results(decrypted_text, found_key)
 
--- lab_1/task2/substitution.py
+++ lab_1/task2/substitution.py
@@ -1,7 +1,13 @@
 from constants import LETTER_FREQUENCIES, ENCRYPTED_ALPHABET
 
 
-def frequency_analysis(encoded_text: str) -> dict:
+def frequency_analysis(encoded_text: str) -> dict[str, float]:
+    """
+    Анализировать частоту появления букв из алфавита зашифрованного текста
+    :param encoded_text: зашифрованный текст
+    :return: словарь, содержащий частоту появления
+        для каждого символа зашифрованного текста
+    """
     freq = {}
     for char in encoded_text:
         if char in ENCRYPTED_ALPHABET:
@@ -15,6 +21,12 @@ def frequency_analysis(encoded_text: str) -> dict:
 
 
 def decrypt(encoded_text: str) -> tuple[str, dict]:
+    """
+    Расшифровать текст согласно частотам появления в нем определенных символов,
+        сопоставляя с частотами появления букв русского алфавита
+    :param encoded_text: зашифрованный текст
+    :return: кортеж, состоящий из расшифрованного текста и ключа для расшифровки
+    """
     encoded_freq = frequency_analysis(encoded_text)
 
     sorted_encoded = sorted(encoded_freq, key=encoded_freq.get, reverse=True)
