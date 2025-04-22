5ebf0924187af913dbaaf86b78ddba47d357201e
radlEve <fadeeveldar2000@gmail.com>
2025-04-22 21:02:15 +0400

    В соответствии с замечаниями, из кода удалены: - Динамическая генерация ключа по частотам (get_frequency_key) - Список ручных правок (CHAR_REPLACEMENTS) - Функция применения ручных правок (apply_manual_corrections)
    Дешифровка теперь выполняется однократным применением константы FINAL_KEY

--- lab_1/task2/main.py
+++ lab_1/task2/main.py
@@ -1,6 +1,6 @@
 from constants import ENCODED_FILE, DECRYPTED_FILE, FOUND_KEY_FILE
-from lab_1.task2.constants import CHAR_REPLACEMENTS
-from substitution import get_frequency_key, decrypt
+from lab_1.task2.constants import FINAL_KEY
+from substitution import decrypt
 
 
 def read_encoded_file() -> str:
@@ -34,41 +34,14 @@ def write_results(decrypted_text: str, key: dict) -> None:
         raise Exception(f"Ошибка записи в {ENCODED_FILE}: {e}")
 
 
-def apply_manual_corrections(key: dict, replacements: list[tuple[str, str]]) -> dict:
-    """
-    Применить ручные изменения для ключа,
-    поочередно меняя местами по 2 буквы из алфавита
-    :param key: исходный ключ (словарь {шифр_буква: обычная_буква})
-    :param replacements: список букв,
-        которые необходимо поменять местами в заданном порядке
-    :return: итоговый ключ
-    """
-    new_key = key.copy()
-
-    for char1, char2 in replacements:
-        key_upd = {}
-        for cipher_char, plain_char in new_key.items():
-            if plain_char == char1:
-                key_upd[cipher_char] = char2
-            elif plain_char == char2:
-                key_upd[cipher_char] = char1
-
-        new_key.update(key_upd)
-
-    return new_key
-
 
 def main():
     try:
         encoded_text = read_encoded_file()
 
-        found_key = get_frequency_key(encoded_text)
+        decrypted_text = decrypt(encoded_text, FINAL_KEY)
 
-        found_key = apply_manual_corrections(found_key, CHAR_REPLACEMENTS)
-
-        decrypted_text = decrypt(encoded_text, found_key)
-
-        write_results(decrypted_text, found_key)
+        write_results(decrypted_text, FINAL_KEY)
 
     except Exception as e:
         print(f"Ошибка: {e}")
